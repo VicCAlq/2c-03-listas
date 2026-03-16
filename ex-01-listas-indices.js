e900d367db79d56a9a6f3a9bae517ba38946d9d7
abner-ferreira-nave <abner.ferreira@nave.org.br>
2026-03-16 11:14:22 -0300

    entregue

--- ex-01-listas-indices.js
+++ ex-01-listas-indices.js
@@ -1,224 +1,202 @@
 /*
-Assunto 01 - Listas e índices
+Assunto 02 - Métodos de listas
 
-Em quaisquer linguagens de programação nós encontramos o conceito de "Estruturas de dados":
-são formas diversas de organizar um conjunto de informações, sejam valores primitivos
-(como strings, números e booleanos), variáveis e constantes, e até mesmo valores mais
-complexos como outras estruturas de dados.
+Para além de manipulação direta de listas utilizando índices, podemos modificá-las
+de outras formas. Listas (também chamados de Arrays) são um tipo de "Objeto" para
+a linguagem JavaScript. Isso significa que para além do valor que atribuímos a elas,
+elas também possuem "propriedades" e "métodos".
 
-A primeira que veremos (novamente) são as "listas" ou "arrays". Se tratam de uma estrutura
-que permite agrupar um conjunto de valores atribuindo a cada um deles uma "posição", como
-a posição de alunos em uma fila.
+Vimos no primeiro exercício sobre a propriedade "length", que nos informa a
+quantidade de itens existentes na lista. Propriedades são simplesmente valores
+associados a objetos.
 
-Para criar uma lista, basta criar uma variável e atribuir a ela um conjunto de valores
-agrupados com colchetes:
+"Métodos" por sua vez são "funções" que executam algo relacionado ao objeto. Lembram
+de "console.log()"?
 
-const minhaLista = ["item 1", "segundo item", "3º", "item quatro"]
+console é um objeto.
+log() é um método do objeto console.
 
-Na lista acima, temos 4 itens. Para acessarmos a lista completa, basta utilizar o nome
-da lista como se faz para qualquer variável:
+A forma de acessarmos o método de um objeto é justamente utilizando o nome do objeto
+(no caso do exercício de agora, o nome da lista) seguido de um ponto, e então o
+nome do método após o ponto, lembrando de abrir os parênteses após o nome do método
+para que este seja executado.
 
-console.log(minhaLista)      // Vai exibir ["item 1", "segundo item", "3º", "item quatro"]
+Vamos aos métodos de lista que veremos hoje. Para os exemplos a seguir, considere
+a lista de exemplo abaixo:
 
-E quando queremos acessar apenas um item da lista? Da mesma forma que em uma fila
-de pessoas podemos referenciar a pessoa pela posição dela na lista (A sexta pessoa
-da lista, levante a mão por favor!), podemos também referenciar os itens de uma lista
-pela posição deles nesta lista. 
+const listaExemplo = ["Este", "Esse", "Aquele", "Aqui", "Aí", "Alí"]
 
-A sintaxe para isso é utilizar o nome da lista seguido do número correspondente
-a posição desejada entre colchetes. Exemplo:
+1. push: O método "push", do verbo "empurrar" em inglês, permite inserir um
+         ou mais itens na lista, sempre no final dela:
 
-minhaLista[1]               // Acessa o valor "segundo item"
+listaExemplo.push("Acolá")
+["Este", "Esse", "Aquele", "Aqui", "Aí", "Alí", "Acolá"]
 
-Notem que quando utilizamos o índice 1, estamos acessando o SEGUNDO item da lista.
-Isso se deve ao fato que em quase toda linguagem de programação (JavaScript incluso)
-índices, sejam de listas ou outras estruturas de dados com índices (como strings)
-são contados começando a partir do zero. Então para acessar o primeiro item, usamos:
+listaExemplo.push("Isto", "Isso", "Aquilo")
+["Este", "Esse", "Aquele", "Aqui", "Aí", "Alí", "Acolá", "Isto", "Isso", "Aquilo"]
 
-minhaLista[0]               // Acessa o valor "item 1"
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-Sendo assim, no exemplo acima de uma lista com quatro itens, o último item é acessado
-usando o índice 3:
+2. pop: O método "pop", do verbo "estourar" em inglês, remove o último item
+        da lista, e retorna este item removido permitindo que ele seja armazenado
+        em uma variável caso desejado:
 
-minhaLista[3]               // Acessa o valor "item quatro"
+listaExemplo.pop()
+["Este", "Esse", "Aquele", "Aqui", "Aí", "Alí", "Acolá", "Isto", "Isso"]
 
-Pergunta: É sempre necessário saber o tamanho da lista para poder acessar itens
-em ordem reversa? É possível começar do último?
+const itemRemovido = listaExemplo.pop()
+["Este", "Esse", "Aquele", "Aqui", "Aí", "Alí", "Acolá", "Isto"]
+itemRemovido === "Isso"
 
-Resposta: Não é necessário saber o tamanho total da lista. Toda lista traz consigo
-além dos valores armazenados nela, algumas propriedades e funções. Podemos descobrir
-o tamanho total da lista usando a propriedade "length", como na sintaxe abaixo:
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-minhaLista.length           // Nos retorna o valor 4
+3. shift: O método "shift", do verbo "deslocar", funciona igual ao método "pop",
+          mas ao invés de fazer isso  com o último item da lista, faz isso com
+          o primeiro item:
 
-Notem que para a quantidade de itens da lista, não se considera a mesma contagem
-que o índice: Uma lista com 4 itens não tem seus itens contados a partir do zero
-por que uma lista pode existir com zero itens.
+listaExemplo.shift()
+["Esse", "Aquele", "Aqui", "Aí", "Alí", "Acolá", "Isto", "Isso"]
 
-E como podemos então acessar o último item de uma lista sem saber o tamanho total
-dela com antecedência? Podemos usar essa propriedade de "length"?
+const exPrimeiroItem = listaExemplo.shift()
+["Aquele", "Aqui", "Aí", "Alí", "Acolá", "Isto", "Isso"]
+exPrimeiroItem === ""Esse"
 
-minhaLista[minhaLista.length]    // Nos retorna undefined
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-Na hora que usamos "minhaLista.length" como valor para o índice, o valor dentro
-dos colchetes é substiituído pelo valor "4". Como vimos anteriormente, índices
-são contados a partir do zero, então quando acessamos minhaLista[4], estamos
-tentando na verdade acessar o QUINTO item, mas esta lista só tem quatro itens.
+4. unshift: O método "unshift", sem tradução direta, faz o "oposto" do que o
+            "shift" faz, ou seja, ele funciona como o "push", mas para o começo
+            da lista. Ele insere um ou mmais valores novos no início da lista:
 
-Se quisermos então acessar o último item da lista usando a propriedade "length"
-podemos simplesmente subtrair 1 do número de itens da lista:
+listaExemplo.unshift("Meu")
+["Meu", "Aquele", "Aqui", "Aí", "Alí", "Acolá", "Isto", "Isso"]
 
-minhaLista[minhaLista.length - 1]      // Acessa o valor "item quatro"
+listaExemplo.unshift("Teu", "Nosso")
+["Teu", "Nosso", "Meu", "Aquele", "Aqui", "Aí", "Alí", "Acolá", "Isto", "Isso"]
 
-Da mesma forma, podemos acessar o penúltimo item com:
-minhaLista[minhaLista.length - 2]      // Acessa o valor "3º"
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-E o antepenúltimo item com:
-minhaLista[minhaLista.length - 3]      // Acessa o valor "segundo item"
+5. concat: O método "concat", do verbo "concatenar" (ligar o final de um item ao
+           começo de outro), faz justamente isso com listas: Une duas ou mais
+           listas, na sequência que forem informadas:
 
-E por aí vai.
+const listaUm = ["a", "b", "c"]
+const listaDois = [1, 2, 3]
+const listaTres = ["#", "@", "&"]
 
-E como podemos modificar essa lista? Simples: Assim como qualquer variável,
-basta acessar o valor e atribuir (com o sinal de =) um valor novo:
+listaUm.concat(listaDois)
+["a", "b", "c", 1, 2, 3]
 
-minhaLista[1] = "2º item"
+listaUm.concat(listaTres, ["nova", "lista", "criada", "na", "hora"])
+["a", "b", "c", 1, 2, 3, "#", "@", "&", "nova", "lista", "criada", "na", "hora"]
 
-Agora quando exibimos a lista completa, ao invés do valor anterior de:
-["item 1", "segundo item", "3º", "item quatro"]
-Temos o valor de:
-["item 1", "2º item", "3º", "item quatro"]
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-E como podemos adicionar um item a mais na lista usando índices?
+ 6. reverse: O método "reverse", do verbo "inverter", faz exatamente isso: ele
+             modifica a lista original invertendo a ordem dos itens:
 
-minhaLista[minhaLista.length] = "item nº 5"
-["item 1", "2º item", "3º", "item quatro", "item nº 5"]
-
-E como podemos trocar dois itens de lugar usando índices? Dois jeitos:
-
-Jeito 1:
-const minhaLista = ["item 1", "2º item", "3º", "item quatro", "item nº 5"]
-let valorTemporario = minhaLista[1]
-minhaLista[1] = minhaLista[3]
-minhaLista[3] = valorTemporario
-
-Resultado da lista:
-["item 1", "item quatro", "3º", "2º item", "item nº 5"]
-
-Expllicação: Para substituir o segundo item com o quarto, criamos uma variável
-provisória para "guardar" o valor do segundo item. Após isso modificamos o
-valor do segundo item para ser igual ao valor do quarto item, e por fim
-modificamos o valor do quarto item para ser igual ao valor original do
-segundo item que estava guardado na variável temporária.
-
-Jeito 2:
-const minhaLista = ["item 1", "2º item", "3º", "item quatro", "item nº 5"]
-[ minhaLista[2], minhaLista[4] ] = [ minhaLista[4], minhaLista[2] ]
-
-Resultado da lista:
-["item 1", "2º item", "item nº 5", "item quatro", "3º"]
-
-Explicação: JavaScript permite atribuir múltiplos valores a múltiplas variáveis,
-mas para que isso funcione é necessário utilizar uma sintaxe chamada "desestruturação":
-Essa sintaxe permite que uma lista ou um objeto sejam "separados" em múltiplas
-variáveis ao mesmo tempo, exemplo:
-
-let [tlocaLetlas, comilona] = ["Cebolina", "Magali", "Mônica", "Cascão"]
-tlocaLetlas      // Recebeu o valor de ["Cebolinha"]
-comilona         // Recebeu o valor de ["Magali"]
-
-Os valores restantes no exemplo acima são descartados. É possível não descartar
-estes valores, mas isso fica para uma próxima lição.
-
+const listaOriginal = [1, 2, 3, 4, 5, 6, 7, 8]
+listaOriginal.reverse()
+[8, 7, 6, 5, 4, 3, 2, 1]
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 ------------------------------------------------------------------------------
 
-Questões 01 a 10: Exercícios com manipulação direta de índices
+Questões 01 a 14: Exercícios com métodos de listas
 
-MÍNIMO NECESSÁRIO PARA NOTA MÁXIMA: 7 questões
+MÍNIMO NECESSÁRIO PARA NOTA MÁXIMA: 10 questões
 */
 
-/* Questão 1
-Crie uma lista (com const) chamado "frutas" com os itens: "maçã", "banana", "laranja", 
-"uva", "pêra", "manga". Armazene na variável "resposta01" o segundo item dessa lista.
+// Questão 1
+const profissoes = [];
+profissoes.push("programador(a)", "designer", "engenheiro(a)", "veterinário(a)", "administrador(a)");
+export const resposta01 = profissoes;
+
+// Questão 2
+const profissaoRemovidaPop = profissoes.pop();
+export const resposta02 = profissaoRemovidaPop;
+
+// Questão 3
+profissoes.unshift("músico(a)");
+export const resposta03 = profissoes;
+
+// Questão 4
+const profissaoRemovidaShift = profissoes.shift();
+export const resposta04 = profissaoRemovidaShift;
+
+// Questão 5
+const removidas = [profissaoRemovidaPop, profissaoRemovidaShift];
+const profissoesConcatenadas = profissoes.concat(removidas);
+// Atualizando a referência para as próximas questões conforme o enunciado sugere continuidade
+profissoes.splice(0, profissoes.length, ...profissoesConcatenadas); 
+export const resposta05 = profissoes;
+
+// Questão 6
+profissoes.reverse();
+export const resposta06 = profissoes;
+
+// Questão 7
+const vertebrados = [];
+const invertebrados = [];
+const animais = [];
+vertebrados.push("elefante", "leopardo", "porco-espinho");
+invertebrados.push("polvo", "formiga", "caranguejo");
+const listaAnimais = animais.concat(vertebrados, invertebrados);
+export const resposta07 = listaAnimais;
+
+// Questão 8 (Invertebrados na frente: polvo, formiga, caranguejo, elefante, leopardo, porco-espinho)
+// Para mover os 3 últimos para a frente usando pop/unshift:
+listaAnimais.unshift(listaAnimais.pop()); // caranguejo
+listaAnimais.unshift(listaAnimais.pop()); // formiga
+listaAnimais.unshift(listaAnimais.pop()); // polvo
+export const resposta08 = listaAnimais;
+
+// Questão 9
+listaAnimais.reverse(); 
+// Move 3 da frente para o final
+listaAnimais.push(listaAnimais.shift());
+listaAnimais.push(listaAnimais.shift());
+listaAnimais.push(listaAnimais.shift());
+export const resposta09 = listaAnimais;
+
+// Questão 10
+const animaisNovos = listaAnimais.concat("carcará", "morcego", "salmão");
+animaisNovos.reverse();
+const listaFinal = animaisNovos.concat("lesma", "borboleta", "ostra");
+export const resposta10 = listaFinal;
 */
-// Array na linha abaixo
-
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta01 = false
-
-/* Questão 2
-Ainda utilizando o array criado na questão 1, armazene na variável "resposta02"
-o último item da lista. Use a propriedade "length" para auxiliar a encontrar
-o índice deste último item.
-*/
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta02 = false
-
-/* Questão 3
-Continuando com o array criado na questão 1, altere o quarto item da lista para
-a fruta "abacaxi". Na variável "resposta03" envie a lista completa com a nova fruta.
-*/
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta03 = false
-
-/* Questão 4
-Ainda com a lista da questão 1, acrescente a fruta "limão" no final da lista, e
-envie a lista inteira na variável "resposta04".
-*/
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta04 = false
-
-/* Questão 5
-Para finalizar com o array da questão 1, troque as posições dos itens "laranja"
-e "manga", e armazene a lista modificada completa na variável "resposta05".
-*/
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta05 = false
-
-/* Questão 6
-Crie um array (também com const ao invés de let) chamado "numeros" com os valores
-7, 2, 4, 9, 8, 6, 1 nesta ordem. Armazene na variável "resposta06" o resultado do
-número no índice zero multiplicado pelo item no penúltimo índice.
-*/
-// Array na linha abaixo
-
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta06 = false
-
-/* Questão 7
-Seguindo com a lista criada na questão 7, utilizando os índices de cada item,
-avance eles em uma posição (e mande o último item para a primeira posição),
-de modo que a lista se torne 1, 7, 2, 4, 9, 8, 6. Armazene a lista na
-variável "resposta07"
-*/
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta07 = false
-
-/* Questão 8
-Continuando com a lista da questão 7, adicione no final da lista o resultado
-da soma de todos os números anteriores da lista. Retorne a lista completa na
-variável "resultado08". É proibido usar métodos de listas e funções.
-*/
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta08 = false
-
-/* Questão 9
-Ainda com a lista da questão 7, mude:
-- O segundo item para a quarta posição
-- O quarto item para a sexta posição
-- O sexto item para a segunda posição
-E retorne a lista completa na variável "resultado09"
-*/
-// Operações necessárias e resposta a partir da linha abaixo
-export const resposta09 = false
-
-/* Questão 10
-Finalizando com a lista da questão 7, crie uma nova lista com três itens onde:
-- O primeiro item da nova lista é o último da anterior
-- O segundo item da nova lista é o penúltimo da anterior
-- O terceiro item da nova lista é o antepenúltimo da anterior
-*/
-// Nova lista abaixo
-
 // Operações necessárias e resposta a partir da linha abaixo
 export const resposta10 = false
+
+/* Questão 11
+Crie (usando const) uma lista vazia chamada "pedras", e use nela o método concat
+para adicionar os itens "quartzo", "basalto" e "granito". Use o método
+reverse nessa lista, e após isso use o método push para adicionar os itens
+"calcário", "mármore" e "mica". Use o método reverse novamente, e envie a lista
+final na resposta11.
+*/
+// Operações necessárias e resposta a partir da linha abaixo
+export const resposta11 = false
+
+/* Questão 12
+Continuando com a lista criada na questão 11, use o método unshift para adicionar
+os itens "jade", "obsidiana" e "zircônio", um de cada vez, na ordem informada na 
+questão. Use o método "concat" para adicionar os itens "topázio", "lazurita" e 
+"ônix" ao final desta lista, e retorne-a na resposta12.
+*/
+// Operações necessárias e resposta a partir da linha abaixo
+export const resposta12 = false
+
+/* Questão 13
+Seguindo com a lista de pedras, use os métodos shift e pop para mover os três
+primeiros itens para o final da lista. Após isso, aplique o método reverse, e
+envie a lista na resposta13.
+*/
+// Operações necessárias e resposta a partir da linha abaixo
+export const resposta13 = false
+
+/* Questão 14
+Finalizando com a lista de pedras, use o método pop para remover os 6 itens finais
+da lista e armazene-os em uma lista nova. Envie esta lista nova na resposta 14.
+*/
+// Operações necessárias e resposta a partir da linha abaixo
+export const resposta14 = false
 
