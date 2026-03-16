32c0b1ca6c48cf735606f208771aaeefb99e0488
davi-neres-nave <davi.neres@nave.org.br>
2026-03-16 11:09:56 -0300

    Update ex-01-listas-indices.js

--- ex-01-listas-indices.js
+++ ex-01-listas-indices.js
@@ -167,7 +167,7 @@ envie a lista inteira na variável "resposta04".
 */
 // Operações necessárias e resposta a partir da linha abaixo
 frutas[frutas.length] = "limão"
-export const resposta04 = fruta
+export const resposta04 = frutas
 
 /* Questão 5
 Para finalizar com o array da questão 1, troque as posições dos itens "laranja"
